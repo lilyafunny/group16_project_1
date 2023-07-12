5364844440ec4ee07d3012c22b06c865a26b4413
Roman <roman.soldatov13@gmail.com>
2023-07-12 12:02:27 +0300

    1207-3
    зробив закриття моб.вікна при переходу по якорям

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeMenuBtn1 = document.querySelector('.js-close-menu1');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,6 +18,7 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn1.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
